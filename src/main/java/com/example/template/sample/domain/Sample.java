bb4283e769ffbd8137d089aa130dd3e31bfc57d2
bbakseong <psm651@naver.com>
2024-02-24 23:06:02 +0900

    template-2 JPA, QuertDSL 연동

--- src/main/java/com/example/template/sample/domain/Sample.java
+++ src/main/java/com/example/template/sample/domain/Sample.java
@@ -5,6 +5,8 @@ import com.example.template.sample.converter.SampleLegacyConverter;
 import com.example.template.sample.enums.SampleLegacy;
 import com.example.template.sample.enums.SampleStatus;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -14,23 +16,27 @@ import java.util.List;
 @Entity
 @Getter
 @NoArgsConstructor
+@Builder
+@AllArgsConstructor
+@Table(name = "samples")
 public class Sample extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(columnDefinition = "name", length = 500, nullable = false)
+    @Column(name = "name", length = 500, nullable = false)
     private String name;
 
     @Enumerated(value = EnumType.STRING)
-    @Column(columnDefinition = "sample_status", nullable = false)
+    @Column(name = "sample_status", nullable = false)
     private SampleStatus sampleStatus;
 
     @Convert(converter = SampleLegacyConverter.class)
-    @Column(columnDefinition = "sample_legacy", nullable = false)
+    @Column(name = "sample_legacy", nullable = false)
     private SampleLegacy sampleLegacy;
 
     @OneToMany(mappedBy = "sample")
+    @Builder.Default
     private List<SampleItem> sampleItemList = new ArrayList<>();
 
 }
